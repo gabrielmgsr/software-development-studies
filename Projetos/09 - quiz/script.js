532b303724cb9c7e20de71753217c35fc8dfde30
MarcosGabriel123 <msantiagorodrigues487@gmail.com>
2026-06-20 11:45:47 -0400

    subir

--- Projetos/09 - quiz/script.js
+++ Projetos/09 - quiz/script.js
@@ -4,9 +4,30 @@ const question = [
         answers:['Konoha', 'Suna', 'Kiri'],
         correct:0
     },
+    {
+        question:'Quem matou CELL em Dragon Ball Z?',
+        answers:['Goku', 'Kuririn', 'Gohan'],
+        correct:2
+    },
+    {
+        question:'Quem entregou o chapéu de palha para Luffy?',
+        answers:['O Rei dos Piratas', 'Shanks', 'Monkey D. Garp'],
+        correct:1
+    },
+    {
+        question:'Qual foi o segundo sensei de Ichigo?',
+        answers:['Kisuke Urahara', 'Rukia Kuchiki', 'Zangetsu'],
+        correct:0
+    },
+    {
+        question:'Qual habilidade principal de Suguru Geto?',
+        answers:['Dez Sombras', 'Proporção 7:3', 'Manipulação de Maldições'],
+        correct:2
+    },
 ];
 
 let current = 0;
+let score = 0;
 
 const questionEl = document.getElementById('question')
 const answersEl = document.getElementById('answers')
@@ -20,6 +41,8 @@ function mostrarPergunta() {
     questionEl.textContent = currentQuestion.question;
     // answersEl.textContent = currentQuestion.answers;
 
+    answersEl.innerHTML = '';
+
     question[current].answers.forEach((AnswersEll, index) =>{
         const botao = document.createElement('button');
         botao.classList.add('botao');
@@ -29,12 +52,25 @@ function mostrarPergunta() {
         botao.addEventListener('click', () =>{
             if (index === question[current].correct){
                 botao.classList.add("correto")
+                score++;
+                scoreEl.textContent = `Pontuação: ${score}`;
             } else{
                 botao.classList.add("errou")
             }
         })
     })
-    
+
+    btnNext.addEventListener('click', () =>{
+        current++;
+
+        if(current < question.length){
+            mostrarPergunta();
+        } else{
+            questionEl.textContent = 'Quiz finalizado!'
+            answersEl.innerHTML = '';
+        }
+
+    })
 }
 
 mostrarPergunta();
