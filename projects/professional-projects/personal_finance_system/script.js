cc23cfcdf530a3373490f06277f0e6918a55fc8b
gabrielmgsr <msantiagororigues@gmail.com>
2026-09-08 00:13:37 -0400

    subir

--- projects/professional-projects/personal_finance_system/script.js
+++ projects/professional-projects/personal_finance_system/script.js
@@ -7,3 +7,22 @@ botao.addEventListener("click", () => {
     sidebar.classList.toggle("fechada");
 
 })
+
+const inputMes = document.getElementById('seletor-mes');
+const titulo = document.getElementById('titulo-financeiro');
+const tituloBase = "Visão Geral Financeira";
+
+inputMes.addEventListener('change', (e) => {
+    const val = e.target.value;
+
+    if (val) {
+        const data = new Date(`${val}-02T00:00:00`);
+
+        const nomeMes = data.toLocaleDateString('pt-BR', { month: 'long'});
+
+        const Mesformatado = nomeMes.charAt(0).toUpperCase() + nomeMes.slice(1);
+        titulo.textContent = `${tituloBase} - ${Mesformatado}`;
+    } else {
+        titulo.textContent = tituloBase;
+    }
+})
